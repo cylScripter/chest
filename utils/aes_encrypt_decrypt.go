9504948ee749b8c2b436fd9b8387b918b98b50c1
cyl <you@example.com>
2024-11-06 14:22:03 +0800

    password

--- utils/aes_encrypt_decrypt.go
+++ utils/aes_encrypt_decrypt.go
@@ -4,152 +4,77 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/rand"
-	"errors"
-	"io"
 )
 
 // Encrypter 加密接口
-type Encrypter interface {
-	Encrypt(plaintext []byte) []byte
-	Decrypt(ciphertext []byte) []byte
+type IEncrypt interface {
+	Encrypt(plaintext []byte) ([]byte, error)
+	Decrypt(ciphertext []byte) ([]byte, error)
 }
 
-type AesEncryptCBC struct {
-	Key []byte
+type AesEncrypt struct {
+	key []byte
 }
 
-func NewAesEncryptCBC(key []byte) *AesEncryptCBC {
-	return &AesEncryptCBC{Key: key}
+func NewAesEncrypt(key []byte) *AesEncrypt {
+	return &AesEncrypt{key: key}
 }
 
-func (cbc *AesEncryptCBC) Encrypt(origData []byte) []byte {
-	// NewCipher creates and returns a new cipher.Block. The key argument should be the AES key, either 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256.
-	block, err := aes.NewCipher(cbc.Key)
-	if err != nil {
-		return nil
-	}
-	//判断加密快的大小
-	blockSize := block.BlockSize()
-	//填充
-	encryptBytes := pkcs7Padding(origData, blockSize)
-	//初始化加密数据接收切片
-	crypted := make([]byte, len(encryptBytes))
-	//使用cbc加密模式
-	blockMode := cipher.NewCBCEncrypter(block, cbc.Key[:blockSize])
-	//执行加密
-	blockMode.CryptBlocks(crypted, encryptBytes)
-	return crypted
-}
-
-// pkcs7Padding 填充
-func pkcs7Padding(data []byte, blockSize int) []byte {
-	//判断缺少几位长度。最少1，最多 blockSize
-	padding := blockSize - len(data)%blockSize
-	//补足位数。把切片[]byte{byte(padding)}复制padding个
-	padText := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(data, padText...)
-}
-
-// pkcs7UnPadding 移除
-func pkcs7UnPadding(data []byte) ([]byte, error) {
-	length := len(data)
-	if length == 0 {
-		return nil, errors.New("加密字符串错误！")
-	}
-	//获取填充的个数
-	unPadding := int(data[length-1])
-	return data[:(length - unPadding)], nil
-}
-
-func (cbc *AesEncryptCBC) Decrypt(encrypted []byte) []byte {
-	block, _ := aes.NewCipher(cbc.Key)                              // 分组秘钥
-	blockSize := block.BlockSize()                                  // 获取秘钥块的长度
-	blockMode := cipher.NewCBCDecrypter(block, cbc.Key[:blockSize]) // 加密模式
-	decrypted := make([]byte, len(encrypted))                       // 创建数组
-	blockMode.CryptBlocks(decrypted, encrypted)                     // 解密
-	decrypted = pkcs5UnPadding(decrypted)                           // 去除补全码
-	return decrypted
-}
-
-func pkcs5Padding(ciphertext []byte, blockSize int) []byte {
-	padding := blockSize - len(ciphertext)%blockSize
-	padText := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(ciphertext, padText...)
-}
-
-func pkcs5UnPadding(origData []byte) []byte {
-	length := len(origData)
-	unfading := int(origData[length-1])
-	return origData[:(length - unfading)]
-}
-
-func AesEncryptECB(origData []byte, key []byte) (encrypted []byte) {
-	newCipher, _ := aes.NewCipher(generateKey(key))
-	length := (len(origData) + aes.BlockSize) / aes.BlockSize
-	plain := make([]byte, length*aes.BlockSize)
-	copy(plain, origData)
-	pad := byte(len(plain) - len(origData))
-	for i := len(origData); i < len(plain); i++ {
-		plain[i] = pad
-	}
-	encrypted = make([]byte, len(plain))
-	// 分组分块加密
-	for bs, be := 0, newCipher.BlockSize(); bs <= len(origData); bs, be = bs+newCipher.BlockSize(), be+newCipher.BlockSize() {
-		newCipher.Encrypt(encrypted[bs:be], plain[bs:be])
-	}
-
-	return encrypted
-}
-func AesDecryptECB(encrypted []byte, key []byte) (decrypted []byte) {
-	newCipher, _ := aes.NewCipher(generateKey(key))
-	decrypted = make([]byte, len(encrypted))
-	//
-	for bs, be := 0, newCipher.BlockSize(); bs < len(encrypted); bs, be = bs+newCipher.BlockSize(), be+newCipher.BlockSize() {
-		newCipher.Decrypt(decrypted[bs:be], encrypted[bs:be])
-	}
-
-	trim := 0
-	if len(decrypted) > 0 {
-		trim = len(decrypted) - int(decrypted[len(decrypted)-1])
-	}
-
-	return decrypted[:trim]
-}
-func generateKey(key []byte) (genKey []byte) {
-	genKey = make([]byte, 16)
-	copy(genKey, key)
-	for i := 16; i < len(key); {
-		for j := 0; j < 16 && i < len(key); j, i = j+1, i+1 {
-			genKey[j] ^= key[i]
-		}
-	}
-	return genKey
-}
-
-func AesEncryptCFB(origData []byte, key []byte) (encrypted []byte) {
+func (a *AesEncrypt) Encrypt(origData []byte) ([]byte, error) {
+	key := a.adjustKeyLength()
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	encrypted = make([]byte, aes.BlockSize+len(origData))
-	iv := encrypted[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
-	}
-	stream := cipher.NewCFBEncrypter(block, iv)
-	stream.XORKeyStream(encrypted[aes.BlockSize:], origData)
-	return encrypted
+	//AES分组长度为128位，所以blockSize=16，单位字节
+	blockSize := block.BlockSize()
+	origData = pKCS5Padding(origData, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize]) //初始向量的长度必须等于块block的长度16字节
+	crypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypted, origData)
+	return crypted, nil
 }
-func AesDecryptCFB(encrypted []byte, key []byte) (decrypted []byte) {
-	block, _ := aes.NewCipher(key)
-	if len(encrypted) < aes.BlockSize {
-		panic("ciphertext too short")
-	}
-	iv := encrypted[:aes.BlockSize]
-	encrypted = encrypted[aes.BlockSize:]
 
-	stream := cipher.NewCFBDecrypter(block, iv)
-	stream.XORKeyStream(encrypted, encrypted)
-	return encrypted
+func (a *AesEncrypt) Decrypt(encrypted []byte) ([]byte, error) {
+	key := a.adjustKeyLength()
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	//AES分组长度为128位，所以blockSize=16，单位字节
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize]) //初始向量的长度必须等于块block的长度16字节
+	origData := make([]byte, len(encrypted))
+	blockMode.CryptBlocks(origData, encrypted)
+	origData = pKCS5UnPadding(origData)
+	return origData, nil
+}
+
+func (a *AesEncrypt) adjustKeyLength() []byte {
+	const keyLength = 16
+	if len(a.key) < keyLength {
+		// 如果密钥长度不足16个字节，在后面补0字节
+		padding := make([]byte, keyLength-len(a.key))
+		return append(a.key, padding...)
+	} else if len(a.key) > keyLength {
+		// 如果密钥长度超过16个字节，只取前16个字节
+		return a.key[:keyLength]
+	}
+	// 密钥长度正好为16个字节，直接返回
+	return a.key
+}
+
+func pKCS5Padding(plaintext []byte, blockSize int) []byte {
+	padding := blockSize - len(plaintext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(plaintext, padtext...)
+}
+
+func pKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	if unpadding > length {
+		return nil
+	}
+	return origData[:(length - unpadding)]
 }
